eb88a4a5c3b9eb73992951d2a77dd7f05024836b
azumahjoshua <joshuaazumah1086@gmail.com>
2022-11-06 23:27:12 +0100

    exercice 1 xml

--- ch13/exo1.py
+++ ch13/exo1.py
@@ -1,5 +1,21 @@
 import urllib.request
-import urllib.parse
-import urllib.error
 import xml.etree.ElementTree as ET
-import ssl
+
+
+# When an application makes a set of services in its API available over the web, we call these web services.
+count = 0
+sum = 0
+url = input('Enter a url:')
+uh = urllib.request.urlopen(url)
+data = uh.read()
+tree = ET.fromstring(data)
+results = tree.findall('comments/comment')
+for item in results:
+    countvalue = int(item.find('count').text)
+    count += 1
+    sum = sum + countvalue
+
+print("Count : ", count)
+print("Sum : ", sum)
+
+# Sample data: http: // py4e-data.dr-chuck.net/comments_42.xml
